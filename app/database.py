731ba358912da5294d6035d86180043ef0e98a05
AnedinodSantos <anedinodsantos@gmail.com>
2022-03-26 10:29:31 -0300

    adicionado arquivo config e ajustado outros arquivos com base nele

---
+++ app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import text, engine_from_config
+from config import config
+
+
+#engine = create_engine("mysql://root:dinossauro12@localhost/mais_fit")
+engine = engine_from_config(config, prefix='db.')
+
+
+def lista_sabores_ativos():
+    with engine.connect() as con:
+        statement = text("""SELECT nome, descricao, valor, link, ativo 
+                            FROM sabores
+                            WHERE ativo = 'SIM'""")
+        rs = con.execute(statement)
+        sabores = []
+        item = rs.fetchone()
+        while (item != None):
+            sabores.append(dict(item))
+            item = rs.fetchone()
+    return sabores
+
+if __name__ == "__main__":
+    print(lista_sabores_ativos())
---
+++ app/servidor.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import database as db
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Home!"
+
+@app.route("/lista")
+def listar_sabores():
+    sabores = {}
+    sabores["sabores"] = db.lista_sabores_ativos()
+    return sabores
+
+
+if __name__ == "__main__":
+    app.run("localhost", port=5000, debug=True)
